e7086bd996c7d1fb4ae20fa0c1b0d64589e62f27
Palmweaver <thepalmweaver@gmail.com>
2019-02-07 06:56:20 -0500

    Initial Commit

---
+++ lib/pixi_projection.rb
@@ -0,0 +1,8 @@
+require "pixi_projection/version"
+
+module PixiProjection
+  class Error < StandardError; end
+ class Engine < ::Rails::Engine;
+  	
+  end
+end
---
+++ lib/pixi_projection/version.rb
@@ -0,0 +1,3 @@
+module PixiProjection
+  VERSION = "0.1.0"
+end
